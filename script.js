1715c9482a25f053fd7e2b9ff52e99153aa51abd
bcrhbrhcdb <apet2804@mpsedu.org>
2024-11-01 18:24:51 -0500

    dd

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ let frameY = 0;
 let gameFrame = 0;
 const staggerFrames = 5;
 function animate(){
-    // ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     // ctx.fillRect(100,50,100,100);
     ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight,
         spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
     if(gameFrame % staggerFrames == 0){
         if(frameX < 6)frameX++;
-        else frameX = 0;
+    else frameX = 0;
     }
     gameFrame++
     requestAnimationFrame(animate);
