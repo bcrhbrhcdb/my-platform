57d84ef3dd3aedb397cbe2ffc0eb6b4630c35f33
bcrhbrhcdb <apet2804@mpsedu.org>
2024-11-01 18:51:27 -0500

    dss

--- script.js
+++ script.js
@@ -11,16 +11,38 @@ const spriteHeight = 523;
 let frameX = 0;
 let frameY = 0;
 let gameFrame = 0;
+const spriteAnimations = [];
+  const animationStates = [
+    {
+    name: "idle",
+    frames: 7,
+
+},
+{
+    name: "jump",
+    frames: 7,
+
+},
+];
+animationStates.forEach((state, index)=>{
+    let frames = {
+        loc: [],
+    }
+    for(let j = 0; j < state.frames; j++){
+        let positionX = j * spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.loc.push({x: positionX, y:positionY});
+    }
+    spriteAnimations[state.name] = frames;
+});
+console.log(animationStates)
 const staggerFrames = 5;
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    // ctx.fillRect(100,50,100,100);
-    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight,
+    let position = Math.floor(gameFrame/staggerFrames) % 6;
+    frameX = spriteWidth * position
+    ctx.drawImage(playerImage, frameX, frameY * spriteHeight,
         spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
-    if(gameFrame % staggerFrames == 0){
-        if(frameX < 6)frameX++;
-    else frameX = 0;
-    }
     gameFrame++
     requestAnimationFrame(animate);
 }
